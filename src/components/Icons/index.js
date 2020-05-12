457eae2d7721b79cf448ab9cd51e48763733cbdd
Sergi Romeu <romeuhd@gmail.com>
2020-05-12 02:26:22 +0200

    add About Icons

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -2,5 +2,7 @@ import GithubIcon from './GithubIcon';
 import LinkedinIcon from './LinkedinIcon';
 import GmailIcon from './GmailIcon';
 import VSCodeIcon from './VSCodeIcon';
+import AboutMeIcon from './AboutMeIcon';
+import AboutMeTitleIcon from './AboutMeTitleIcon';
 
-export { GithubIcon, LinkedinIcon, GmailIcon, VSCodeIcon };
+export { GithubIcon, LinkedinIcon, GmailIcon, VSCodeIcon, AboutMeIcon, AboutMeTitleIcon };
