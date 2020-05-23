e065d8584b107b5806fdc7d9b695cdcb86dd1217
Sergi Romeu <romeuhd@gmail.com>
2020-05-23 21:42:39 +0200

    add menu icon

---
+++ src/components/shared/Icons/MenuIcon.js
@@ -0,0 +1,8 @@
+const MenuIcon = () => (
+  <svg viewBox="0 0 24 24" fill="#ddd" width="18" height="18">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+  </svg>
+);
+
+export default MenuIcon;
