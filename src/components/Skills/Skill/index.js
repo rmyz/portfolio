d83f1209babbcaf066272d013106bab9bb973803
Sergi Romeu <romeuhd@gmail.com>
2020-07-17 17:03:16 +0200

    add animation on skills

--- src/components/Skills/Skill/index.js
+++ src/components/Skills/Skill/index.js
@@ -1,18 +1,34 @@
 import PropTypes from 'prop-types';
+import { useInView } from 'react-intersection-observer';
+import { Animate } from 'react-simple-animate';
 
 import { Wrapper, TextWrapper, Title, Desc, IconWrapper } from './styles';
 
 const Skill = ({ title, desc, Icon }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
   return (
-    <Wrapper>
-      <IconWrapper>
-        <Icon />
-      </IconWrapper>
-      <TextWrapper>
-        <Title>{title}</Title>
-        <Desc>({desc})</Desc>
-      </TextWrapper>
-    </Wrapper>
+    <Animate
+      play={inView}
+      duration="0.5"
+      delay="0.3"
+      start={{ opacity: 0 }}
+      end={{ opacity: 1 }}
+      complete={{ transition: 'all .5s linear 0s' }}
+      render={({ style }) => (
+        <Wrapper ref={ref} style={style}>
+          <IconWrapper>
+            <Icon />
+          </IconWrapper>
+          <TextWrapper>
+            <Title>{title}</Title>
+            <Desc>({desc})</Desc>
+          </TextWrapper>
+        </Wrapper>
+      )}
+    ></Animate>
   );
 };
 
